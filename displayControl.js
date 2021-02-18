1b4fd39ddd43fb20376e097648481670aa56efdd
dltldn333 <62665220+dltldn333@users.noreply.github.com>
2021-02-18 18:40:07 +0900

    Add files via upload

--- displayControl.js
+++ displayControl.js
@@ -1,23 +1,27 @@
-const change_view = $('#change_view') //보기방식 변경
-
-//블럭 버튼 클릭 시
-$('#block_align_btn').click(function(){
-    $('#block_align_btn').css('fill', '#191919');
-    $('#list_align_btn').css('fill', '#E1E1EB');
-    change_view.addClass('block_view');
-    change_view.removeClass('list_view');
-});
-
-//리스트 버튼 클릭 시
-$('#list_align_btn').click(function(){
-    $('#list_align_btn').css('fill', '#191919');
-    $('#block_align_btn').css('fill', '#E1E1EB');
-    change_view.addClass('list_view');
-    change_view.removeClass('block_view');
-});
+//Fast tuch 라이브러리 사용
+$(function() {
+    FastClick.attach(document.body);
+    });
+    
+(function() {
+    const change_view = $('#change_view') //보기방식 변경
+    //블럭 버튼 클릭 시
+    $('#block_align_btn').click(function(){
+        $('#block_align_btn').css('fill', '#191919');
+        $('#list_align_btn').css('fill', '#E1E1EB');
+        change_view.addClass('block_view');
+        change_view.removeClass('list_view');
+    });
+    //리스트 버튼 클릭 시
+    $('#list_align_btn').click(function(){
+        $('#list_align_btn').css('fill', '#191919');
+        $('#block_align_btn').css('fill', '#E1E1EB');
+        change_view.addClass('list_view');
+        change_view.removeClass('block_view');
+    });
+}())
 
 //모바일 버튼 제어
-
 window.addEventListener('scroll', function(){
     var logo_button = document.querySelector("#logo_btn")
     logo_button.classList.toggle("btn_mode_off", window.scrollY > 0)
